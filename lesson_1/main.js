914cb6bfa2c6fbe9a267b511abbef92b3745903d
vantcde130099 <vantcde130099@fpt.edu.vn>
2021-03-15 22:05:10 +0700

    day 1

---
+++ lesson_1/main.js
@@ -0,0 +1,50 @@
+var fullName = 'Tran Cong Van';
+var age = 22;
+
+alert(fullName);
+alert(age);
+
+confirm('Xac nhan');
+prompt('Xac nhan');
+
+setTimeout(function() {
+    alert('thong bao');
+}, 1000);   //1000ms
+
+setInterval(function () {
+   console.log("dem ne " + Math.random()); 
+}, 1000);
+/**
+ * Giới thiệu 1 số hàm built-in
+ *  1.Alert
+ *  2.Console
+ *  3.Confirm
+ *  4.Prompt
+ *  5.Set timeout
+ *  6.Set interval
+ */
+
+/**
+ * Giới thiệu về toán tử trong JS
+ *  1. Toán tử số học - Arithmetic
+ *  2. Toán tử gán - Assignment
+ *  3. Toán tử so sánh - Comparison
+ *  4. Toán tử logic - Logical
+ */
+
+/**
+ * Chuyển sang boolean = true
+ *  1. 0
+ *  2. false
+ *  3. '' - ""
+ *  4. undefined
+ *  5. NaN
+ *  6. null
+ */
+
+/**
+ * Toán tử logic
+ *  1. && - And
+ *  2. || - Or
+ *  3. ! - Not
+ */
