d4a09cd4a9b531c6096e5247864628c6d05fc854
vantcde130099 <vantcde130099@fpt.edu.vn>
2021-04-28 23:48:51 +0700

    done play / pause / seek song

--- Music_Player/app.js
+++ Music_Player/app.js
@@ -14,12 +14,22 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const cd = $('.cd');
+const heading = $('header h2')
+const cdThumb = $('.cd-thumb')
+const audio = $('#audio')
+const playBtn = $('.btn-toggle-play')
+const player = $('.player')
+const progress = $('#progress')
+
 const app = {
+    currentIndex: 0,
+    isPlaying: false,
     songs: [{
             name: '7 years',
             singer: 'Lukas Graham',
             path: './assets/music/7 Years - Lukas Graham [Lossless_FLAC].flac',
-            image: './assets/img/7 years'
+            image: './assets/img/7 years.jpg'
         },
         {
             name: 'All around the world',
@@ -47,9 +57,79 @@ const app = {
         },
     ],
     render: function() {
-        console.log(123);
+        const htmls = this.songs.map(song => {
+            return `
+            <div class="song">
+                <div class="thumb" style="background-image: url('${song.image}')">
+                </div>
+                <div class="body">
+                    <h3 class="title">${song.name}</h3>
+                    <p class="author">${song.singer}</p>
+                </div>
+                <div class="option">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+            </div>
+        `
+        })
+        $('.playlist').innerHTML = htmls.join('');
+    },
+    defineProperties: function() {
+        Object.defineProperty(this, 'currentSong', {
+            get: function() {
+                return this.songs[this.currentIndex]
+            }
+        })
+    },
+    handleEvent: function() {
+        const cdWidth = cd.offsetWidth;
+        document.onscroll = function() {
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
+            const newCdWidth = cdWidth - scrollTop;
+            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0;
+            cd.style.opacity = newCdWidth / cdWidth;
+        }
+
+        playBtn.onclick = function() {
+                if (app.isPlaying) {
+                    audio.pause()
+                } else {
+                    audio.play()
+                }
+            }
+            //khi bai hat dc play
+        audio.onplay = function() {
+                app.isPlaying = true;
+                player.classList.add('playing')
+            }
+            //khi bai hat dc pause
+        audio.onpause = function() {
+                app.isPlaying = false
+                player.classList.remove('playing')
+            }
+            //khi tien do thay doi
+        audio.ontimeupdate = function() {
+                const progressPercent = Math.floor(audio.currentTime / audio.duration * 100)
+                progress.value = progressPercent
+            }
+            //xu ly tua nhac
+        progress.onchange = function(e) { //e la phan tram
+            const seek = audio.duration / 100 * e.target.value
+            audio.currentTime = seek
+        }
+    },
+    loadCurrentSong: function() {
+
+
+        heading.textContent = this.currentSong.name;
+        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
+        audio.src = this.currentSong.path
     },
     start: function() {
+        this.defineProperties();
+        this.handleEvent();
+        this.loadCurrentSong()
         this.render();
     }
 }
+app.start();
